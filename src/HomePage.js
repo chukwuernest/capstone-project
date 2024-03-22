986d5e6f98e0ca1692849eace9b341a0aeb03642
Leochukwu <chukwu17mc@gmail.com>
2024-03-21 23:15:43 -1100

    building a website for female support and development

---
+++ src/HomePage.js
@@ -0,0 +1,172 @@
+import React from 'react'
+import logo from './img/LOGO.svg'
+import image_k from './img/Frame 1000011687.svg'
+import image_w from './img/Frame 1000011685.svg'
+import { IoMdContacts } from 'react-icons/io'
+
+function HomePage() {
+  return (
+    <div className='homepage'>
+      <div className='section'>
+        {/* this is the nav code */}
+        <nav className='navbar'>
+          <img src={logo} alt='logo' />
+          <div className='mainNav'>
+            <ul>
+              <li>
+                <a href='#'>Home</a>
+              </li>
+              <li>
+                <a href='#'>About</a>
+              </li>
+              <li>
+                <a href='#'>Services</a>
+              </li>
+              <li>
+                <a href='#'>Contact</a>
+              </li>
+            </ul>
+          </div>
+          <div className='otherNav'>
+            <ul>
+              <li>
+                <a href='#'>report case</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        {/* here is for the image and also positioning */}
+        <div className='homeI'>
+          <div className='first'>
+            <img src='../try.try/try.jpg' alt='first' />
+          </div>
+          <section className='note'>
+            <h2>
+              We empower survivors to reclaim their dignity, voice, and agency.
+            </h2>
+            <h4>More than 230 resolved cases</h4>
+          </section>
+          <div className='second'>
+            <img src='../try.try/try.jpg' alt='second' />
+          </div>
+        </div>
+        {/* this is the know more about us part */}
+        <div className='moreAboutUs'>
+          <div className='knowingMore'>
+            <section id='lineHead'>
+              <p id='line'></p>
+              <h4>Know About us</h4>
+            </section>
+            <div className='infor_know'>
+              <h3>
+                We provide support for anyone going through any form of abuses{' '}
+              </h3>
+              <p>
+                Our mission is to offer a safe haven for those who have
+                experienced such trauma, fostering a nurturing environment where
+                survivors can heal and rebuild their lives. We strive to empower
+                survivors to reclaim their dignity, voice, and agency.
+              </p>
+              <p>
+                Our team of trained professionals and volunteers work tirelessly
+                to ensure that survivors receive the care, respect, and justice
+                they deserve. Together, we stand as a beacon of hope,
+                solidarity, and compassion, advocating for a world free from
+                violence and oppression.
+              </p>
+              <a href='#' id='learn_more'>
+                learn more
+              </a>
+            </div>
+          </div>
+          <div className='imageKnow'>
+            <img src={image_k} alt='know' />
+          </div>
+        </div>
+        {/* this is foe sponsors */}
+        <div className='sponsor'>
+          <div className='sponsor_h'>
+            <h3>OUR SUPPORTERS</h3>
+            <p></p>
+          </div>
+          <div className='sponsor_I'>
+            <div className='try1'>
+              <img src='' alt='sponsor1' srcset='sponsor1' />
+              <p>john</p>
+            </div>
+            <div className='try2'>
+              <img src='' alt='sponsor2' srcset='sponsor2' />
+              <p>jams</p>
+            </div>
+            <div className='try3'>
+              <img src='' alt='sponsor2' srcset='sponsor2' />
+              <p>jams</p>
+            </div>
+            <div className='try4'>
+              <img src='' alt='sponsor2' srcset='sponsor2' />
+              <p>jams</p>
+            </div>
+            <div className='try5'>
+              <img src='' alt='sponsor2' srcset='sponsor2' />
+              <p>jams</p>
+            </div>
+            <div className='try6'>
+              <img src='' alt='sponsor2' srcset='sponsor2' />
+              <p>jams</p>
+            </div>
+          </div>
+        </div>
+        {/* what we do */}
+        <div className='what_W_D'>
+          <div className='what'>
+            <section id='wHead'>
+              <p id='line'></p>
+              <h4>what we do</h4>
+            </section>
+            <div className='what_k'>
+              <h3>Some services we provide to Abused Victims</h3>
+              <p>
+                <IoMdContacts />
+                Counselling:we offer one-on-one sessions to abuse victims to
+                help them process their experiences, understand their emotions,
+                and develop coping strategies. .
+              </p>
+              <p>
+                <IoMdContacts />
+                Legal Advocacy: We help victims ito identify their rights,
+                discuss legal options available to them, and provide guidance on
+                how to navigate the legal system.
+              </p>
+              <p>
+                <IoMdContacts />
+                Medical Support: We collaborate with healthcare professionals
+                and other support services to provide comprehensive care.
+              </p>
+              <p>
+                <IoMdContacts />
+                Creating Safe Spaces: We create safe and confidential
+                environments where abuse victims can come together to share
+                their experiences, emotions, and challenges without fear of
+                judgment or retaliation.
+              </p>
+            </div>
+          </div>
+          <div className='what_i'>
+            <img src={image_w} alt='what' />
+          </div>
+        </div>
+        {/* project we have done */}
+        <div className='project_d'>
+          <div className='project'>
+            <section id='projectHead'>
+              <p id='project_p'></p>
+              <h4>Projects we have done</h4>
+            </section>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default HomePage
